416cc0e2deb92ebbdfe4d4f0bd671a9c05f11d56
Michelle Cifuentes <j.micro.0101@gmail.com>
2021-06-17 13:26:04 -0600

    Se creo la clase Prestamo

---
+++ src/peliculas/Prestamo.java
@@ -0,0 +1,18 @@
+package src.peliculas;
+
+import src.clientes.*;
+
+public class Prestamo {
+    
+    private String[] prestamos;
+
+    public void prestarPelicula(VectorClientes vc, VectorPeliculas vp) {
+
+        vc.mostrarClientes();
+
+
+
+    }
+
+
+}
